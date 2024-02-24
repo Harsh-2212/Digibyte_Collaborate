67e693d454fe56e4b0123f5f543534f2399fc15c
Harsh Vishwakarma <harshvish2003@gmail.com>
2024-02-24 15:04:41 +0530

    hostevent

--- Agnethon/app.py
+++ Agnethon/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
+@app.route("/hostaevent")
+def hostaevent():
+    return render_template("hostaevent.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
